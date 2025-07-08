fd97921fd8d9430679c7e937306dfd7ba9d4d4c6
veryAngryKitten <kjobbagyi23@gmail.com>
2025-07-08 20:05:19 +0200

    Database stuff mainly

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const app = express()
 
+const Database = require('better-sqlite3');
+
+// This will create 'mydatabase.db' if it doesn't exist
+const db = new Database('users.db');
+
+// You can now create tables
+db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        password TEXT,
+        money REAL
+    )
+`);
+
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('dashboard')
+})
+
+app.get('/welcome', (req, res) => {
+    res.render('landing_page')
 })
 
 app.post('/deposit', (req, res) => {
